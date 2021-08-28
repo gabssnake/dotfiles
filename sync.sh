023215fb8c001b8841818a277323a285d5297a92
Gabriel VIVAS <gabriel.vivas@stormshield.eu>
2021-08-28 22:17:58 +0200

    adds stash option to sync script

--- sync.sh
+++ sync.sh
@@ -13,6 +13,14 @@ fileset=(
   .profile
   .prompt
   .vimrc
+  # Legacy files
+  .bash_profile
+  .bash_aliases
+  .gitignore_global
+  .npm-completion.bash
+  .node-completion.bash
+  .git-completion.bash
+  .mocharc.yml
 )
 
 function sync {
@@ -21,8 +29,8 @@ function sync {
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         for filename in "${fileset[@]}"; do
             echo "  $filename"
-            rm "$2/$filename"
-            cp "$1/$filename" "$2/$filename"
+            rm "$2/$filename" 2>/dev/null
+            cp "$1/$filename" "$2/$filename" 2>/dev/null
         done
         echo 'Done.'
     else
@@ -36,9 +44,15 @@ if [ "$1" == "--install" ]; then
 elif [ "$1" == "--backup" ]; then
     echo 'This will backup your dotfiles (without commit).'
     sync "$HOME" "$dir"
+elif [ "$1" == "--stash" ]; then
+    echo 'This will stash your current dotfiles.'
+    stash="$dir/stash-$(date +%F-%s)"
+    mkdir -p "$stash"
+    sync "$HOME" "$stash"
 else
     echo 'Usage:'
     echo '  $ sync --install'
     echo '  $ sync --backup'
+    echo '  $ sync --stash'
 fi
 
