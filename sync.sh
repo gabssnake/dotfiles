d4cd5c3f9b5ce2227a7f787593e58ca9abc60b73
gabssnake <gabssnake@gmail.com>
2022-01-05 12:13:51 +0100

    tweak sync script to print usage by default

--- sync.sh
+++ sync.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-
 fileset=(
   .aliases
   .bashrc
@@ -24,35 +22,59 @@ fileset=(
 )
 
 function sync {
-    read -p "From '$1' to '$2'. Proceed? (y/n) " -r -n 1;
-    printf '\n'
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        for filename in "${fileset[@]}"; do
-            printf '  %s\n' "$filename"
-            rm "$2/$filename" 2>/dev/null
-            cp "$1/$filename" "$2/$filename" 2>/dev/null
-        done
-        printf 'Done.\n'
-    else
-        printf 'Aborted.\n'
-    fi
+  read -p "From '$1' to '$2'. Proceed? (y/n) " -r -n 1;
+  printf '\n'
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    for filename in "${fileset[@]}"; do
+      printf '  %s\n' "$filename"
+      rm "$2/$filename" 2>/dev/null
+      cp "$1/$filename" "$2/$filename" 2>/dev/null
+    done
+    printf 'Done.\n'
+  else
+    printf 'Aborted.\n'
+  fi
 }
 
-if [ "$1" == "--install" ]; then
-    printf 'This will overwrite dotfiles in your home directory.\n'
-    sync "$dir" "$HOME"
-elif [ "$1" == "--backup" ]; then
-    printf 'This will backup your dotfiles (without commit).\n'
-    sync "$HOME" "$dir"
-elif [ "$1" == "--stash" ]; then
-    printf 'This will stash your current dotfiles.\n'
-    stash="$dir/stash-$(date +%F-%s)"
-    mkdir -p "$stash"
-    sync "$HOME" "$stash"
-else
-    printf 'Usage:\n'
-    printf '  $ sync --install    # Use the dotfiles from this repo.\n'
-    printf '  $ sync --backup     # Save the dotfiles from this machine.\n'
-    printf '  $ sync --stash      # Save a dated directory from this machine.\n'
-fi
+function usage {
+  echo "Usage:
+    $ sync --install    # Use the dotfiles from this repo.
+    $ sync --backup     # Save the dotfiles from this machine.
+    $ sync --stash      # Save a dated directory from this machine."
+  exit 0;
+}
+
+function main {
+  # help when no arguments or options
+  if [[ $# == 0 ]]; then usage; fi
+
+  # extract options from arguments
+  opts=(); while :; do if [[ $1 == -?* ]]; then opts+=("$1"); else break; fi; shift; done
+
+  # expects to be run from the dotfiles repo
+  dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+  for opt in "${opts[@]}"; do
+    case "$opt" in
+      -i|--install)
+        printf 'This will overwrite dotfiles in your home directory.\n'
+        sync "$dir" "$HOME"
+        break;;
+      -b|--backup)
+        printf 'This will backup your dotfiles (without commit).\n'
+        sync "$HOME" "$dir"
+        break;;
+      -s|--stash)
+        printf 'This will stash your current dotfiles.\n'
+        stash="$dir/stash-$(date +%F-%s)"
+        mkdir -p "$stash"
+        sync "$HOME" "$stash"
+        break;;
+      *)
+        usage;;
+    esac
+  done
+}
+
+main "$@"
 
